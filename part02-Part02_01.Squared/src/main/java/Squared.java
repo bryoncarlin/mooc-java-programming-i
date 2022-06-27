92f3d4d3a437e7b6ea355de9de1100059869dee0
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-06-26 22:45:56 -0400

    6/26

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -5,6 +5,12 @@ public class Squared {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int first = Integer.valueOf(scanner.nextLine());
+        
+        
+        int third = first * first;
+        System.out.println(third);
+        
 
     }
 }
