80c7a41b3bbe13f52ac1e116873c8bad181272ec
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-06-26 23:41:13 -0400

    6/26

--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -5,6 +5,14 @@ public class AbsoluteValue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int num = Integer.valueOf(scanner.nextLine());
+        if (num > 0) {
+            System.out.println(num * 1);
+        }
+        if (num < -1) {
+            System.out.println(num * -1);
+
+        }
 
     }
 }
