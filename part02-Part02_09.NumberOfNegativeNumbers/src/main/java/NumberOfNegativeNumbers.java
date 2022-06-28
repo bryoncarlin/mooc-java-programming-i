7905c5efe661434d02534c30f72dc6f2b41cbfa9
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-06-27 22:28:21 -0400

    6/27

--- part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -5,6 +5,19 @@ public class NumberOfNegativeNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int num = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int bee = scanner.nextInt();
+            if (bee == 0) {
+                System.out.println("Number of negative numbers: " + num);
+                break;
+            }
+            if (bee < 0) {
+                num = num + 1;
 
+            }
+
+        }
     }
 }
--- part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -5,6 +5,17 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int num = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int bee = scanner.nextInt();
+            if (bee == 0) {
+                System.out.println("Sum of the numbers: " + num);
+                break;
+            }
+            num = num + bee;
+
+        }
 
     }
 }
