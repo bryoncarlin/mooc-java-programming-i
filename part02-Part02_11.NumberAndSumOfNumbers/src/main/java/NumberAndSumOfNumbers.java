7989bca770e6a1f32c26681460151d6cd3aa2c90
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-06-27 22:41:36 -0400

    6/27

--- part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -5,6 +5,19 @@ public class NumberAndSumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int num = 0;
+        int sum = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int bee = scanner.nextInt();
+            if (bee == 0) {
+                System.out.println("Number of numbers: " + num);
+                System.out.println("Sum of the numbers: " + sum);
+                break;
+            }
+            num = num + 1;
+            sum = bee + sum;
+        }
 
     }
 }
