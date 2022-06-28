df582023dff1ee870400652406b85f18e42c568a
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-06-28 17:08:44 -0400

    6/28

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,20 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double num = 0;
+        double rift = 0;
+        double average = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int bee = scanner.nextInt();
+            if (bee == 0) {
+                System.out.println("Average of the numbers: " + average);
+                break;
+            }
+            num = num + bee;
+            rift = rift + 1;
+            average = num / rift;
 
+        }
     }
 }
