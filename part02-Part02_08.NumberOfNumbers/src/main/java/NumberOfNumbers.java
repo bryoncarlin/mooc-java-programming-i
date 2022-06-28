863f99b66eb84446edc51600ccc3a304ddef0da5
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-06-27 21:39:35 -0400

    6/27

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,18 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int bee = scanner.nextInt();
+            if (bee == 0) {
+                System.out.println("Number of numbers: " + number);
 
+                break;
+            }
+
+            number = number + 1;
+
+        }
     }
 }
