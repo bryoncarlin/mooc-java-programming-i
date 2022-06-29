7b3721f0c318ad2d459433d3db698d51915c96a8
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-06-29 00:00:27 -0400

    6/28

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,25 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int bee = scanner.nextInt();
 
+        int sum = 0;
+        int number = 0;
+
+        while (bee != 0) {
+            if (bee > 0) {
+                number++;
+                sum += bee;
+
+            }
+            bee = scanner.nextInt();
+        }
+        double average = (double) sum / number;
+
+        if (sum == 0) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println(average);
+        }
     }
 }
