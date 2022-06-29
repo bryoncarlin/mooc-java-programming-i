558548cbe7fa02019b132c76692478fe447be69b
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-06-29 15:17:10 -0400

    6/29

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -6,10 +6,9 @@ public class FromWhereToWhere {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-       
         System.out.print("Where to? " + "\n");
         int bee = scanner.nextInt();
-        System.out.println("Where from?" + "\n");
+        System.out.println("Where from?");
         int bees = scanner.nextInt();
         for (int i = bees; i <= bee; i++) {
             System.out.print(i + "\n");
