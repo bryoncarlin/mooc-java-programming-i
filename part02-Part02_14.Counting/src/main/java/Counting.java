5e3832c20ef5ab4ef96aa67ef8c85e24ffd696d5
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-06-29 11:29:33 -0400

    6/29

--- part02-Part02_14.Counting/src/main/java/Counting.java
+++ part02-Part02_14.Counting/src/main/java/Counting.java
@@ -5,6 +5,10 @@ public class Counting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int bee = scanner.nextInt();
+        for(int i = bee; i < 5; i++){
+            System.out.println(i);
+        }
 
     }
 }
