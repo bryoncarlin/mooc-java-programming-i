5511ca465f1a1f20f0dde6d90421566739b71987
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-06-29 21:46:20 -0400

    6/29

--- part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -5,6 +5,13 @@ public class SumOfASequence {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Last Number?");
+        int bee = scanner.nextInt();
+        int sum = 0;
+        for (int i = 0; i <= bee; i++) {
+            sum = sum + i;
 
+        }
+        System.out.println("The sum is " + sum);
     }
 }
