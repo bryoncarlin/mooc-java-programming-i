ed2addb882e35132d0bd39959e68093ff414b5d6
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-06-29 22:13:21 -0400

    6/29

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,14 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int bee = scanner.nextInt();
+        int honey = 1;
+        for (int i = 1; i <= bee; i++) {
+            honey = honey * i;
+
+        }
+        System.out.println("Factorial:" + honey);
 
     }
 }
