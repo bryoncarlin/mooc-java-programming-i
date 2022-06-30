218af23da25b2d3e71302349b7bbaf0e2e5a862f
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-06-29 22:01:15 -0400

    6/29

--- part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -5,6 +5,17 @@ public class SumOfASequenceTheSequel {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("First Number?");
+        int bee = scanner.nextInt();
+        System.out.println("Last Number?");
+        int bees = scanner.nextInt();
+        int sum = 0;
+        for (int i = bee; i <= bees; i++) {
+            sum += i;
+            
 
+        }
+       
+        System.out.println("The sum is " + sum);
     }
 }
