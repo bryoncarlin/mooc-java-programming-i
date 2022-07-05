75beed8a4e07313cedd07fa898b468a873caf1e3
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-07-05 16:15:27 -0400

    07/5/22

--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -4,15 +4,38 @@ import java.util.Scanner;
 public class RepeatingBreakingAndRemembering {
 
     public static void main(String[] args) {
-        
-        // This exercise is worth five exercise points, and it is 
-        // gradually extended part by part.
-        
-        // If you want, you can send this exercise to the server
-        // when it's just partially done. In that case the server will complain about 
-        // the parts you haven't done, but you'll get points for the finished parts.
-        
-        Scanner scanner = new Scanner(System.in);
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give numbers:");
+        int sum = 0;
+        int numbers = 0;
+        double avg = 0;
+        int even = 0;
+        int odd = 0;
+
+        while (true) {
+            int bee = scanner.nextInt();
+            if (bee <= -1) {
+                break;
+            }
+            if (bee <= 0) {
+                continue;
+
+            }
+            if (bee % 2 == 1) {
+                odd++;
+            } else if (bee % 2 == 0) {
+                even++;
+            }
+            sum += bee;
+            numbers++;
+
+        }
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + numbers);
+        System.out.println("Average " + (double) sum / numbers);
+        System.out.println("Even " + even);
+        System.out.println("Odd " + odd);
     }
 }
