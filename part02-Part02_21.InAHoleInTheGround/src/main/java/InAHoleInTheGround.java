2a9c181fc3eb0e929ad6042ca6b66d4978d138c5
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-07-05 16:38:40 -0400

    07/05/2022

--- part02-Part02_21.InAHoleInTheGround/src/main/java/InAHoleInTheGround.java
+++ part02-Part02_21.InAHoleInTheGround/src/main/java/InAHoleInTheGround.java
@@ -1,10 +1,17 @@
+
+import java.util.Scanner;
+
 public class InAHoleInTheGround {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         printText();
+
     }
-    
+
     public static void printText() {
+
         System.out.println("In a hole in the ground there lived a method");
     }
 }
