186a152acaa54cbb3debbcac54e2c88747d7f294
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-07-09 12:37:44 -0400

    7/9/22

--- part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -5,7 +5,20 @@ public class DivisibleByThree {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int bee= scanner.nextInt();
+        int bees = scanner.nextInt();
+        divisibleByThreeInRange(bee, bees);
+        
 
     }
 
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = beginning; i <= end; i++) {
+            if (i % 3 == 0) {
+                System.out.print(i + "\n");
+            }
+
+        }
+
+    }
 }
