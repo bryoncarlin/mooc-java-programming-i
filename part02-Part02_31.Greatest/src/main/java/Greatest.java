03c470098f8f7a75fd65feb8d86f19b058ec2dde
bryoncarlin <49820180+bryoncarlin@users.noreply.github.com>
2022-07-13 17:36:42 -0400

    7/13

--- part02-Part02_31.Greatest/src/main/java/Greatest.java
+++ part02-Part02_31.Greatest/src/main/java/Greatest.java
@@ -2,9 +2,9 @@
 public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
-        //write some code here
+        
 
-        return -1;
+        return Math.max(number1, Math.max(number2, number3)) ;
     }
 
     public static void main(String[] args) {
